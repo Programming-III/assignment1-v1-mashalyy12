9201e8887b2229f2f135ea2a7572882dcbc9f15c
mashalyy12 <yassinamr582@gmail.com>
2025-11-12 15:08:11 +0200

    Implement animal class with derived classes
    Define the animal class with properties and inheritance for mammal, bird, and reptile.

--- Animal.h
+++ Animal.h
@@ -4,7 +4,36 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+public class animal{
+    
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    this -> name = name;
+    this -> age = age;
+    this -> isHungry = isHungry;
+    
+
+
+public mammal :: public animal{
+     string furColor;
+    
+  }
+
+    public bird :: public animal{
+        float wingSpan;
+        
+    }
+    
+    public reptile :: public animal{
+        bool isVenomous;
+        
+    }
+};
+    
 
 
 
